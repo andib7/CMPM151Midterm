17831c4d9df2ec87a35bc7d11b84d099b8aff85b
Andi Barron <andibarron7@gmail.com>
2023-05-01 15:26:59 -0700

    Initial Commit

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovePlayer : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+
+	private Rigidbody rb;
+	private int count;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+	}
+	
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//countText.text = "(x,y,z): (" + rb.position.x.ToString() + ", " + rb.position.y.ToString() + ", " + rb.position.z.ToString() + ")";
+
+
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			setCountText ();
+		}
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+	}
+		
+}
---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour {
+
+	//Vector3 vect;
+	// Use this for initialization
+	void Start () {
+		//vect = new Vector3 (50, 0, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right * 50 * Time.deltaTime);
+
+		//Vector3.right
+		//Vector23
+
+		//transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}
